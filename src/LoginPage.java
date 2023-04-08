f5fe4c064a985f626a1b25254aa6c18ad1044623
azizullahayoubi <azizullahayoubi19@gmail.com>
2023-04-08 12:49:28 -0400

    login page code

---
+++ src/LoginPage.java
@@ -0,0 +1,10 @@
+public class LoginPage {
+
+    public static void main(String[] args) {
+
+        String userName="Noor";
+        String password="pass123";
+
+        System.out.println("UserName is "+userName+" password is "+password);
+    }
+}
